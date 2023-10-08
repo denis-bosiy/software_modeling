25868ec518bb406feff0bac467923729fa2a39cb
denis.bosyi <denis.bosiy@gmail.com>
2023-10-09 01:10:25 +0300

    Добавлены правки по 1-й лабе

--- first_lw/run.bat
+++ first_lw/run.bat
@@ -1,3 +1,3 @@
-docker build -t first_lw .
-docker run -dp 127.0.0.1:8080:8080 --name first_container --rm first_lw
-docker run -dp 127.0.0.1:8081:8080 --name second_container --rm first_lw
+docker build -t first-lw:1.0.0 .
+docker run -dp 127.0.0.1:8080:8080 --name 2048-game-v1 --rm first-lw:1.0.0
+docker run -dp 127.0.0.1:8081:8080 --name 2048-game-v2 --rm first-lw:1.0.0
